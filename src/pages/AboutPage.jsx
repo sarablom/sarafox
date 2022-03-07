4190bf75fd3ba66ccc60569c8db7d3d9df6e8ff3
sarablom <sara.cecilia.blom@gmail.com>
2022-03-07 12:22:04 +0100

    change about

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ function AboutPage() {
     return (
         <section className="about-main">
             <h1>Om mig</h1>
-            <p>Jag heter Sara Blom och bor i Göteborg. Mina största fritidsintressen och passioner är att sy och att göra hemsidor. På den här sidan samlar jag mina verk, både vad jag har sytt och vilka sidor jag har kodat. Tidigare har jag haft ett eget företag och sålt kläder via det, men just nu har jag inget aktivt företag längre, men du kan fortfarande skicka en förfrågan om det är någon tjänst du vill beställa av mig.</p>
+            <p>Jag heter Sara Blom och bor i Göteborg. Mina största fritidsintressen och passioner är att sy och att göra hemsidor. På den här sidan samlar jag mina verk, både vad jag har sytt och vilka sidor jag har kodat. Tidigare har jag haft ett eget företag och sålt kläder via det, men just nu har jag inget aktivt företag längre. Du kan fortfarande skicka en förfrågan om det är någon tjänst du vill beställa av mig.</p>
         </section>
     )
 }
