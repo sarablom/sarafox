39b941d27883e09cca3bb9ce168ca65ef2a0804e
sarablom <sara.cecilia.blom@gmail.com>
2022-03-17 15:40:20 +0100

    add responsive behaviour

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function AboutPage() {
-    return (
-        <section className="about-main">
-            <h1>Om mig</h1>
-            <p>Jag heter Sara Blom och bor i Göteborg. Mina största fritidsintressen och passioner är att sy och att göra hemsidor. På den här sidan samlar jag mina verk, både vad jag har sytt och vilka sidor jag har kodat. Tidigare har jag haft ett eget företag och sålt kläder via det, men just nu har jag inget aktivt företag längre. Du kan fortfarande skicka en förfrågan om det är någon tjänst du vill beställa av mig.</p>
-        </section>
-    )
+  return (
+    <section className="about-main">
+      <h1>Om mig</h1>
+      <p className="about-main__para">
+        Jag heter Sara Blom och bor i Göteborg. Det roligaste som finns är att sy och att göra hemsidor.
+        Tidigare har jag sålt kläder via ett eget företag, men det gör jag inte längre. 
+        När jag har gjort den här sidan har jag fokuserat på att skapa ett verktyg för att kunna beställa barnkläder 
+        i olika tyger. Sidan är kodad i JavaScript med ramverket React.
+      </p>
+    </section>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,10 @@ function HomePage() {
     return (
         <section className="home-main">
             <h1>Välkommen till Sara Fox Barnkläder</h1>
-            <p>Här kan du designa dina egna kläder genom att välja mellan olika modeller och tyger.</p>
+            <p>Här kan du designa dina egna kläder genom att välja mellan olika modeller och tyger.
+                För att läsa om projektet kan du gå in på länken "Om", men det allra coolaste innehållet hittar du 
+                under fliken "Produkter".
+            </p>
         </section>
     )
 }
