ffc3ad5b6bc4d67f7a28e13987ebf6f0c79a0419
sarablom <sara.cecilia.blom@gmail.com>
2022-03-17 16:04:23 +0100

    synk to GH

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
-    return (
-        <section className="home-main">
-            <h1>Välkommen till Sara Fox Barnkläder</h1>
-            <p>Här kan du designa dina egna kläder genom att välja mellan olika modeller och tyger.
-                För att läsa om projektet kan du gå in på länken "Om", men det allra coolaste innehållet hittar du 
-                under fliken "Produkter".
-            </p>
-        </section>
-    )
+  return (
+    <section className="home-main">
+      <h1>Välkommen till Sara Fox Barnkläder</h1>
+      <p>
+        Här kan du designa dina egna kläder genom att välja mellan olika
+        modeller och tyger. För att läsa om projektet kan du gå in på länken
+        "Om", men det allra coolaste innehållet hittar du under fliken
+        "Produkter".
+      </p>
+    </section>
+  );
 }
 
-export default HomePage
+export default HomePage;
